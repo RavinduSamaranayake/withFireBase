d1bfaf19297c695d48b4d4f48f2501850f7e2e99
RavinduSamaranayake <ovkravindu95@gmail.com>
2018-09-27 16:21:24 +0530

    try to auth

---
+++ app/src/main/java/com/example/user/withfirebase/MainPgActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.withfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pg);
+    }
+}
